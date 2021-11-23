bd5d166978da04f585fba6041e9647a00bec85f6
Ksor Au <autinn123@gmail.com>
2021-11-23 22:05:06 +0700

    Initial setup

---
+++ server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+
+const indexRouter = require('./routes/index');
+
+
+var mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true }).then(function () {
+        console.log("Successfully connected to the database");
+    }).catch(function (err) {
+        console.log('Could not connect to the database. Exiting now...', err);
+        process.exit();
+    });
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
+
+
+app.use('/', indexRouter);
+
+app.listen(process.env.PORT || 3000);
