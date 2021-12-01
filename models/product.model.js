bb3a335b3fedef8d000f8dc77a42e1c95cb0d44c
autinn123 <autinn123@gmail.com>
2021-12-01 14:50:50 +0700

    style

--- models/product.model.js
+++ models/product.model.js
@@ -7,7 +7,9 @@ const productSchema = new Schema({
     description: { type: String, required: true },
     category: { type: String, required: true },
     price: { type: String, required: true },
-    status: { type: Number, required: true, default: 1 }
+    discount: { type: Number, required: 0, default: 0 },
+    status: { type: Number, required: true, default: 1 },
+    activeFlag: { type: Number, required: 0, default: 1 }
 });
 
 module.exports = mongoose.model('product', productSchema);
